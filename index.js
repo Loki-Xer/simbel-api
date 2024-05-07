2951938bb2a9892784c4fa1d4ab1bdfe5fd69905
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-05-07 20:08:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
-    return res.send('API is online...!!');
+    return res.send('<div style="background-color: black; color: black;">API is online...!!</div>');
 });
 
 app.get('/search', async (req, res) => {
